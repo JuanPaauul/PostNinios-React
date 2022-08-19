aede28f771655133fd9235391a29b6c70d6773e8
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-08-19 18:02:22 -0400

    Addion Axios package

---
+++ src/components/PostForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Axios from 'axios';
+
+PostForm.propTypes = {
+  
+};
+
+function PostForm(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default PostForm;
